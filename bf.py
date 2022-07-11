20687b0a292f12dc3fc9f3266d6862fb08c1b997
yeong-hwan <jghff700@naver.com>
2022-07-12 00:13:27 +0900

    Upload bf(binance_functions)

---
+++ bf.py
@@ -0,0 +1,122 @@
+import pandas as pd
+import time
+
+
+# ----------------- binance functions -----------------------
+
+'''
+ohclv: candle data
+standard_date
+    -1: today(now)
+    -2: yesterday / before
+'''
+
+
+def get_RSI(ohlcv, period, standard_date):
+    ohlcv["close"] = ohlcv["close"]
+    delta = ohlcv["close"].diff()
+    up, down = delta.copy(), delta.copy()
+    up[up < 0] = 0
+    down[down > 0] = 0
+    _gain = up.ewm(com=(period - 1), min_periods=period).mean()
+    _loss = down.abs().ewm(com=(period - 1), min_periods=period).mean()
+    RS = _gain / _loss
+    return float(pd.Series(100 - (100 / (1 + RS)), name="RSI").iloc[standard_date])
+
+
+def get_MA(ohclv, period, standard_date):
+    close = ohclv["close"]
+    ma = close.rolling(period).mean()
+    return float(ma[standard_date])
+
+
+# period: (1d,4h,1h,15m,10m,1m ...)
+def get_ohlcv(binance, ticker, period):
+    btc_ohlcv = binance.fetch_ohlcv(ticker, period)
+    ohlcv = pd.DataFrame(btc_ohlcv, columns=[
+                         'datetime', 'open', 'high', 'low', 'close', 'volume'])
+    ohlcv['datetime'] = pd.to_datetime(ohlcv['datetime'], unit='ms')
+    ohlcv.set_index('datetime', inplace=True)
+    return ohlcv
+
+
+# cut_rate: (1.0: -100% clearing, 0.1: -10% stop loss)
+def set_stop_loss(binance, ticker, cut_rate):
+    time.sleep(0.1)
+
+    orders = binance.fetch_orders(ticker)
+
+    stop_loss_ok = False
+    for order in orders:
+
+        if order['status'] == "open" and order['type'] == 'stop_market':
+            # print(order)
+            stop_loss_ok = True
+            break
+
+    # if no stop loss => take order
+    if stop_loss_ok == False:
+
+        time.sleep(10.0)
+
+        balance = binance.fetch_balance(params={"type": "future"})
+        time.sleep(0.1)
+
+        amt = 0
+        entry_price = 0
+        leverage = 0
+
+        for position in balance['info']['positions']:
+            if position['symbol'] == ticker.replace("/", ""):
+                entry_price = float(position['entry_price'])
+                amt = float(position['position_amt'])
+                leverage = float(position['leverage'])
+
+        # if long, get short posiotion
+        side = "sell"
+        # if short, get long posiotion
+        if amt < 0:
+            side = "buy"
+
+        danger_rate = ((100.0 / leverage) * cut_rate) * 1.0
+
+        # stop loss price - long
+        stop_price = entry_price * (1.0 - danger_rate * 0.01)
+
+        # stop loss price - short
+        if amt < 0:
+            stop_price = entry_price * (1.0 + danger_rate * 0.01)
+
+        params = {
+            'stop_price': stop_price,
+            'close_position': True
+        }
+
+        print("side:", side, "   stop_price:",
+              stop_price, "   entry_price:", entry_price)
+        # stop loss order
+        print(binance.create_order(ticker, 'STOP_MARKET',
+              side, abs(amt), stop_price, params))
+
+        print("--------------- STOPLOSS SETTING DONE ---------------")
+
+
+# amount for buying
+def get_amount(usdt, coin_price, rate):
+
+    target = usdt * rate
+
+    amount = target / coin_price
+
+    if amount < 0.001:
+        amount = 0.001
+
+    #print("amout", amout)
+    return amount
+
+
+def get_coin_current_price(binance, Ticker):
+    coin_info = binance.fetch_ticker(Ticker)
+    coin_price = coin_info['last']  # coin_info['close'] == coin_info['last']
+
+    return coin_price
--- binance_trading_main.py
+++ binance_trading_main.py
@@ -3,18 +3,61 @@ import time
 import pandas as pd
 import pprint
 
-access = "UGZCnsSs7qGO57JbAnBaEECKp6Te1paWsdSk3tnGkbP04ZQLokQOvKsF9Ls23TR1"
-secret = "2sVI8v2N2j0E4yoQZWkIVmzpopWNHxxYbx0yI9XqaVpIGMZKPsvtNMwjSPIot9ad"
+access = "Zd2awwadeB2BFrxvs3tLQixrpVtkM8PfvvCVZAHeaF1RSYWckSOdUfJvwt6elXeF"
+secret = "9pumuiCRihNdUTY7BO2d0sRZrBaeII7oERqNe7kDpqKoL9pVDVLQlMSsLFqZ4CGb"
 
 binance = ccxt.binance(config={
-    'apikey': access,
+    'apiKey': access,
     'secret': secret,
-    'enable_rate_limit': True,
+    'enableRateLimit': True,
     'options': {
-        'default_type': 'future'
+        'defaultType': 'future'
     }
 })
 
 target_coin_ticker = "BTC/USDT"
+target_coin_symbol = "BTCUSDT"
+
 btc = binance.fetch_ticker(target_coin_ticker)
+btc_price = btc['close']
+
 pprint.pprint(btc)
+
+
+# taker_short
+# binance.create_market_sell_order(target_coin_ticker, 0.001)
+
+# taker_long
+# binance.create_market_buy_order(target_coin_ticker, 0.001)
+
+# maker_short
+# binance.create_limit_sell_order(target_coin_ticker, 0.001, btc_price)
+
+balance = binance.fetch_balance(params={"type": "future"})
+pprint.pprint(balance)
+print(balance['USDT'])
+
+for position in balance['info']['positions']:
+    if position['symbol'] == target_coin_symbol:
+        leverage = float(position['leverage'])
+        entry_price = float(position['entryPrice'])
+        unrealized_profit = float(position['unrealizedProfit'])
+        amt = float(position['positionAmt'])
+
+'''
+entry_price: average buy price
+amt: amount of position
+'''
+
+print("amt:", amt)
+print("entry_price:", entry_price)
+print("leverage:", leverage)
+print("unrealized_profit:", unrealized_profit)
+
+# short to long (sell position)
+abs_amt = abs(amt)
+
+# long sell
+entry_price_long = entry_price * 1.001
+# short sell
+entry_price_short = entry_price * 0.999
